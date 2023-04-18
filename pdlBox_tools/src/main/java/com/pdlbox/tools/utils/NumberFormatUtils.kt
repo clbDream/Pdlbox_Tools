d2c36d819cc36e4b36e56ce1a3ab8492728c71dd
3214341986 <aBC123987@>
2023-04-18 19:25:09 +0800

    增加数字格式化工具类

---
+++ pdlBox_tools/src/main/java/com/pdlbox/tools/utils/NumberFormatUtils.kt
@@ -0,0 +1,54 @@
+package com.pdlbox.tools.utils
+
+import java.math.BigDecimal
+
+/**
+ * @ClassName: NumberFormatUtils
+ * @Description: 数字格式化工具类
+ * @Author: 常利兵
+ * @Date: 2023/4/18 18:53
+ **/
+object NumberFormatUtils {
+
+    /**
+     * 数字格式化(保留1位小数）
+     */
+    fun numberFormat(i: Long): String {
+        return if (i < 1000) {
+            (i.toInt()).toString()
+        } else if (i in 1000..9999) {
+            val tempNum = (i / 1000F).toString()
+            tempNum.substring(0, tempNum.indexOf(".", 0) + 2) + "k"
+        } else if (i in 10000..9999999) {
+            val tempNum = (i / 10000F).toString()
+            tempNum.substring(0, tempNum.indexOf(".", 0) + 2) + "w"
+            String.format("%.1f", i / 10000F) + "w"
+        } else {
+            //kw
+            val tempNum = (i / 10000000F).toString()
+            tempNum.substring(0, tempNum.indexOf(".", 0) + 2) + "kw"
+        }
+    }
+
+    /**
+     * 保留两位小数(去掉最后的0）
+     */
+    fun keepTwoDecimalPlaces(number: String?): String {
+        try {
+            var number = number ?: return ""
+            if (number.indexOf(".") > 0) {
+                //正则表达
+                number = number.replace("0+?$".toRegex(), "") //去掉后面无用的零
+                number = number.replace("[.]$".toRegex(), "") //如小数点后面全是零则去掉小数点
+            }
+            return if (number.indexOf(".") > 0) {
+                val b = BigDecimal(number)
+                b.setScale(2, BigDecimal.ROUND_HALF_UP).toDouble().toString()
+            } else {
+                number
+            }
+        } catch (e: Exception) {
+            return "0"
+        }
+    }
+}
---
+++ pdlBox_tools/src/test/java/com/pdlbox/tools/NumberFormatTest.kt
@@ -0,0 +1,29 @@
+package com.pdlbox.tools
+
+import com.pdlbox.tools.utils.NumberFormatUtils
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * @ClassName: NumberFormatTest
+ * @Description:
+ * @Author: 常利兵
+ * @Date: 2023/4/18 19:00
+ **/
+class NumberFormatTest {
+
+    @Test
+    fun testNumberFormat(){
+        Assert.assertEquals("1.9k", NumberFormatUtils.numberFormat(1999))
+        Assert.assertEquals("1.0w", NumberFormatUtils.numberFormat(10000))
+        Assert.assertEquals("9.9k", NumberFormatUtils.numberFormat(9999))
+        Assert.assertEquals("999", NumberFormatUtils.numberFormat(999))
+    }
+
+    @Test
+    fun keepTwoDecimalPlacesTest(){
+        Assert.assertEquals("10.59",NumberFormatUtils.keepTwoDecimalPlaces("10.59254"))
+        Assert.assertEquals("10.6",NumberFormatUtils.keepTwoDecimalPlaces("10.596"))
+        Assert.assertEquals("0",NumberFormatUtils.keepTwoDecimalPlaces("10.596as"))
+    }
+}
